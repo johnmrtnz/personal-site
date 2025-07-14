1068c97d218d016b923b1748c3af6227956fbb5a
johnmrtnz <74370157+johnmrtnz@users.noreply.github.com>
2025-07-14 11:20:21 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,9 @@ import Footer from './components/Footer';
 import './App.css';
 // import { HashRouter as Router } from 'react-router-dom';
 
-// const App: React.FC = () => {
-//   return (
-//     <Router>
-//       <div className="app">
-//         <Navbar />
-//         <main className="main-content">
-//           <Routes>
-//             <Route path="/" element={<Home />} />
-//           </Routes>
-//         </main>
-//         <Footer />
-//       </div>
-//     </Router>
-//   );
-// };
-
 const App: React.FC = () => {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename="/personal-site">
       <div className="app">
         <Navbar />
         <main className="main-content">
@@ -38,5 +22,21 @@ const App: React.FC = () => {
     </Router>
   );
 };
+
+// const App: React.FC = () => {
+//   return (
+//     <Router basename={process.env.PUBLIC_URL}>
+//       <div className="app">
+//         <Navbar />
+//         <main className="main-content">
+//           <Routes>
+//             <Route path="/" element={<Home />} />
+//           </Routes>
+//         </main>
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// };
 
 export default App;
