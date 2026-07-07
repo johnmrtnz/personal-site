0edf8a066507a49791494134f3efc75c883864bc
johnmrtnz <74370157+johnmrtnz@users.noreply.github.com>
2026-07-06 21:33:03 -0400

    Setting up domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://johnmrtnz.github.io',
-  base: '/personal-site',
+  site: 'https://johnmartinez.xyz',
+  base: '/',
   integrations: [react()],
 });
